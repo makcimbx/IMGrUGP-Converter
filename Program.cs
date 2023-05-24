e5445ed81ff117efc969eb36b8ad9a2d5a99f0f8
Makcim Ivanov <makcimbx@gmail.com>
2023-05-24 18:27:36 +0300

    English + Usability Upgrade

--- Program.cs
+++ Program.cs
@@ -3,31 +3,66 @@ using System.Drawing;
 
 if (args.Length == 0)
 {
-    Console.WriteLine("Не хватает аргументов! В качестве аргумента могут быть папка или изображение.");
+    Console.WriteLine("Argument not enough exception! 1: Input Image or Folder; 2: Output Folder;");
     Console.ReadLine();
     return;
 }
 
 var arg = args[0];
+var output = args.Length > 1 ? args[1] : string.Empty;
+
 if (File.Exists(arg))
 {
-    var image = Image.FromFile(arg);
-    var newPath = Path.ChangeExtension(arg, "rip");
-    WriteImage(image, newPath);
+    try
+    {
+        var image = Image.FromFile(arg);
+        var newPath = Path.ChangeExtension(arg, "rip");
+        if (!string.IsNullOrEmpty(output))
+        {
+            var fileName = Path.GetFileName(newPath);
+            WriteImage(image, Path.Combine(output, fileName));
+        }
+        else
+        {
+            WriteImage(image, newPath);
+        }
+    }
+    catch
+    {
+        Console.WriteLine($"Can't convert file {arg}");
+    }
 }
 else if (Directory.Exists(arg))
 {
     var directory = Directory.GetFiles(arg);
+    if (string.IsNullOrEmpty(output))
+    {
+        output = Path.Combine(arg, "Converted");
+    }
+
+    if (!Directory.Exists(output))
+    {
+        Directory.CreateDirectory(output);
+    }
+
     foreach (var file in directory)
     {
-        var image = Image.FromFile(file);
-        var newPath = Path.ChangeExtension(file, "rip");
-        WriteImage(image, newPath);
+        try
+        {
+            var image = Image.FromFile(file);
+            var newPath = Path.ChangeExtension(file, "rip");
+            var fileName = Path.GetFileName(newPath);
+            WriteImage(image, Path.Combine(output, fileName));
+        }
+        catch
+        {
+            Console.WriteLine($"Can't convert file {arg}");
+        }
     }
 }
 else
 {
-    Console.WriteLine("Неправильные аргументы! В качестве аргумента могут быть папка или изображение.");
+    Console.WriteLine("Argument not image or folder! 1: Input Image or Folder; 2: Output Image or Folder;");
     Console.ReadLine();
 }
 
